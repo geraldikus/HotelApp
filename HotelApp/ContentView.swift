2c6b92f0c50ff8cd7f6ec6da34263811c463d159
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-08-31 11:32:29 +0400

    Initial commit

---
+++ HotelApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HotelApp
+//
+//  Created by Anton on 31.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotelApp/HotelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelAppApp.swift
+//  HotelApp
+//
+//  Created by Anton on 31.08.23.
+//
+
+import SwiftUI
+
+@main
+struct HotelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
